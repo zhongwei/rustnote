3b941aef22842634d506e27a55b07c0bcd224a2e
Zhong Wei <zhongwei99@163.com>
2018-02-06 21:13:29 +0800

    add custom types sample

---
+++ 003/structures.rs
@@ -0,0 +1,36 @@
+struct Nil;
+
+struct Pair(i32, f32);
+
+struct Point {
+    x: f32,
+    y: f32,
+}
+
+#[allow(dead_code)]
+struct Rectangle {
+    p1: Point,
+    p2: Point,
+}
+
+fn main() {
+    let point: Point = Point { x: 0.3, y: 0.4};
+
+    println!("point coordinates: ({}, {})", point.x, point.y);
+
+    let Point { x: my_x, y: my_y } = point;
+
+    let _rectangle = Rectangle {
+        p1: Point { x: my_y, y: my_x},
+        p2: point,
+    };
+
+    let _nil = Nil;
+
+    let pair = Pair(1, 0.1);
+    println!("pair contains {:?} and {:?}", pair.0, pair.1);
+
+    let Pair(integer, decimal) = pair;
+    println!("pair contains {:?} and {:?}", integer, decimal);
+
+}
