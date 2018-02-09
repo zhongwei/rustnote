9d16c4ceb3e948e6c476bff2dc7c9e3d2f3a62d2
Zhong Wei <zhongwei99@163.com>
2018-02-09 22:43:28 +0800

    add flow control sample

---
+++ 008/if_let_enum.rs
@@ -0,0 +1,23 @@
+enum Foo {
+    Bar,
+    Baz,
+    Qux(u32)
+}
+
+fn main() {
+    let a = Foo::Bar;
+    let b = Foo::Baz;
+    let c = Foo::Qux(100);
+
+    if let Foo::Bar = a {
+        println!("a is foobar");
+    }
+
+    if let Foo::Bar = b {
+        println!("b is foobar");
+    }
+
+    if let Foo::Qux(value) = c {
+        println!("c is {}", value);
+    }
+}
