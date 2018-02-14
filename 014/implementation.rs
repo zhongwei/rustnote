f31a0af6a28545f97bfa2b7f54a3de523ed2f9d3
Wei Zhong <zhongwei99@163.com>
2018-02-14 23:51:29 +0800

    add generics samples

---
+++ 014/implementation.rs
@@ -0,0 +1,30 @@
+struct S;
+struct GenericaVal<T>(T,);
+
+impl GenericaVal<f32> {}
+impl GenericaVal<S> {}
+
+impl <T> GenericaVal<T> {}
+
+struct Val {
+    val: f64
+}
+
+struct GenVal<T> {
+    gen_val: T
+}
+
+impl Val {
+    fn value(&self) -> &f64 { &self.val }
+}
+
+impl <T> GenVal<T> {
+    fn value(&self) -> &T { &self.gen_val }
+}
+
+fn main() {
+    let x = Val { val: 3.0 };
+    let y = GenVal { gen_val: 3i32 };
+
+    println!("{}, {}", x.value(), y.value());
+}
