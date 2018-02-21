48ab06863740252a8ca1bfde8d68e8d04956c954
Zhong Wei <zhongwei99@163.com>
2018-02-21 13:59:58 +0800

    add macro samples

---
+++ 017/DSL.rs
@@ -0,0 +1,18 @@
+macro_rules! calculate {
+    (eval $e:expr) => {{
+        {
+            let val: usize = $e;
+            println!("{} = {}", stringify!{$e}, val);
+        }
+    }};
+}
+
+fn main() {
+    calculate! {
+        eval 1 + 2
+    }
+
+    calculate! {
+        eval (1 + 2) * (3 / 4)
+    }
+}
